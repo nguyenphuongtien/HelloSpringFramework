ff27e144905f95a9601a36b8fbff38546f7afb56
Nguyễn Phương Tiền <nphtien.tgg@vnpt.vn>
2020-02-04 09:57:41 +0700

    finish spring app hello

--- src/main/java/org/o7planning/spring/bean/MyComponent.java
+++ src/main/java/org/o7planning/spring/bean/MyComponent.java
@@ -1,5 +1,17 @@
 package org.o7planning.spring.bean;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MyComponent {
+	
+	@Autowired
+	private MyRepository repo;
+	
+	public void showAppInfo() {
+		System.out.println("Now is: " + repo.getSystemDateTime());
+		System.out.println("App Name: " + repo.getAppName());
+	}
 
 }
